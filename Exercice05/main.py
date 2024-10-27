49db1e5dfaa9af35d9cf9e2925f9428d03714035
glen <glen84_dr@hotmail.com>
2024-10-27 15:36:27 +0100

    solutin of half of exercices

--- Exercice05/main.py
+++ Exercice05/main.py
@@ -1,5 +1,14 @@
+
+'''
+Fonction that will return the sum between
+two parametres a and b
+'''
 def sum(a, b):
     return a + b
- 
+
+'''
+Fonction that will return the difference between
+two parametres a and b
+'''
 def subtraction(a, b):
     return a - b
--- Exercice06/main.py
+++ Exercice06/main.py
@@ -1,4 +1,6 @@
 # Fonction calculate_average
+def calculate_average(numbers):
+    return sum(numbers)/len(numbers)
  
 # Exemple d'utilisation de la fonction
 numbers = [10, 20, 30, 40, 50]
