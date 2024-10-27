f7907fba5c26ae6dbaeadc498dbbcbcf2bf53227
glen <glen84_dr@hotmail.com>
2024-10-27 15:28:16 +0100

    Resolution 3 fist exercices

--- Exercice02/main.py
+++ Exercice02/main.py
@@ -15,3 +15,16 @@ students = {
          'Histoire': 78
      }
 }
+
+name = input("Entrez le nom de l'étudiant : ")
+if name not in  students.keys():
+     print(f" L'étudiant {name} n'existe pas dans la liste.")
+else:
+     student = students[name]
+     print(f"Notes de {name} :")
+     total = 0
+     for k, note in student.items():
+          print(f"{k} : {note}")
+          total += note
+
+     print(f"Moyenne de {name} : {float(total)/3}")
--- Exercice03/main.py
+++ Exercice03/main.py
@@ -1,1 +1,12 @@
 words = ["python", "programmation", "langage", "ordinateur", "apprentissage"]
+voyels = ['a','e','i','o','u','y']
+
+def count_voyels(word):
+    i = 0
+    for w in word:
+        if w in voyels:
+            i += 1
+    return i
+
+tuple_words = [(w,count_voyels(w)) for w in words]
+print(tuple_words)
