5df1a15681a447d9f016463be35a987f4a97dc09
glen <glen84_dr@hotmail.com>
2024-10-28 09:09:58 +0100

    exercice with inheritance class

--- Exercice10/main.py
+++ Exercice10/main.py
@@ -1,1 +1,25 @@
-## Écrivez votre code ici !
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display_details(self):
+        print(f"Mr/Miss {self.name} is {self.age} years old!")
+
+class Employee(Person):
+    def __init__(self, name, age, salary):
+        super().__init__(name, age)
+        self.salary = salary
+
+    
+    def display_details(self):
+        super().display_details()
+        print(f"And his monthly salary is {self.salary} euros")
+
+print(">>>>> Person part <<<<<<")
+person = Person("Aqif", 33)
+person.display_details()
+print("_______________")
+print(">>>>> Employ part <<<<<<")
+emp = Employee("Aqif", 22, 3000)
+emp.display_details()
