3c31def11ae65ed12833b0109a2be40543e6b4ef
glen <glen84_dr@hotmail.com>
2024-10-28 09:49:55 +0100

    finished exc 11

--- Exercice11/main.py
+++ Exercice11/main.py
@@ -1,1 +1,19 @@
 ## Écrivez votre code ici !
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+
+    def withdraw(self, amount):
+        self.balance = self.balance - amount
+
+    def display_balance(self):
+        print(f"Welcome Mr/Miss {self.account_holder} you have {self.balance} on your account!")
+
+bianca = BankAccount("Bianca Tropdarg", 10000)
+bianca.deposit(1000)
+bianca.withdraw(500)
+bianca.display_balance()
