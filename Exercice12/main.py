37870bd2b56e9acbcd0f691df0e2f1c2b3c8515e
glen <glen84_dr@hotmail.com>
2025-01-15 10:21:39 +0100

    Correction performance last exercise

--- Exercice12/main.py
+++ Exercice12/main.py
@@ -5,6 +5,10 @@ def refacto_search_book(list_books, book_title):
         if book.title == book_title:
             return book
 
+def refacto_error(books, book_title):
+    if books.get(book_title) is None:
+        raise ValueError("Wrong title, please try again!")
+
 
 class Book:
     def __init__(self, title, author, year):
@@ -13,41 +17,45 @@ class Book:
         self.author = author
         self.year = year
 
+
 class Library:
     def __init__(self, books=None, borrowed_books=None):
-        self.books = books or []
-        self.borrowed_books = borrowed_books or []
+        self.books = books or {}
+        self.borrowed_books = borrowed_books or {}
     
     def add_book(self, book):
-        self.books.append(book)
+        self.books[book.title] = book
 
-
+    # Remove book from the library
     def remove_book(self, book_title):
-        book = refacto_search_book(self.books, book_title)
-        if not book:
-            raise ValueError("No book found")
-        self.books.remove(book)
+        refacto_error(self.books, book_title)
+        self.books.pop(book_title)
 
+    # Borrow book from library
     def borrow_book(self, book_title):
-        book = refacto_search_book(self.books, book_title)
-        self.borrowed_books.append(book)
+        refacto_error(self.books, book_title)
+        self.borrowed_books[book_title] = self.books.get(book_title)
 
+    # Returning book on the library
     def return_book(self, book_title):
-        book = refacto_search_book(self.books, book_title)
-        self.borrowed_books.remove(book)
+        refacto_error(self.books, book_title)
+        if self.borrowed_books.get(book_title) is None:
+            raise ValueError("The book is not borrowed")
 
+        self.borrowed_books.pop(book_title)
+
+    # Return the list of Available books
     def available_books(self):
         print("The avaible books of the Library are: ")
-        all_books = self.books
-        for book in all_books:
+        for k, book in self.books.items():
             if book not in self.borrowed_books:
-                print(f"{book.title} - {book.author} ");\
+                print(f"{k} - {book.author} ")
 
 
     def borrowed_books_m(self):
         print("The borrowed books of the Library are:")
-        for book in self.borrowed_books:
-            print(f"{book.title} - {book.author} ")
+        for k, book in self.borrowed_books.items():
+            print(f"{k} - {book.author} ")
 
 
 book1 = Book(title="The mist", author="Shekspare", year=2000)
@@ -64,9 +72,6 @@ library.add_book(book3)
 library.add_book(book4)
 library.add_book(book5)
 
-# Exemple
-library2 = Library()
-print("Test... ", library2.__dict__)
 
 # remove book
 library.remove_book("Book to delete")
@@ -74,6 +79,12 @@ library.remove_book("Book to delete")
 # Borrowing books
 library.borrow_book("The Sun")
 library.borrow_book("The mist")
+library.borrow_book("The mist 2")
+
+# Returning book
+library.return_book('The mist 2')
+print("   ")
+print(library.__dict__)
 
 # Borrowed books
 library.borrowed_books_m()
